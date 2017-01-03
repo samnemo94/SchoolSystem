d3c5eda311c974e1502131e46bb098b659d7dbf0
Samy <nnssllqqppp@gmail.com>
2017-01-03 02:15:12 +0200

    cat image

--- frontend/views/site/category.php
+++ frontend/views/site/category.php
@@ -4,9 +4,17 @@
     {
         ?>
         <div class="item">
-            <div class="ui tiny image">
-                <img src="<?= $row['image']['value'] ?>">
-            </div>
+            <?php
+            if (array_key_exists('image',$row))
+            {
+                ?>
+                <div class="ui tiny image">
+                    <img src="<?= $row['image']['value'] ?>">
+                </div>
+            <?php
+            }
+            ?>
+
             <div class="middle aligned content">
                 <a class="header"><?= $row['title']['value'] ?></a>
             </div>
